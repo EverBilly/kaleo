87aa7b7558f48a37b4c68e17f9635525156a9d39
Ever Cifuentes <31419663+EverBilly@users.noreply.github.com>
2025-07-08 22:16:23 +0200

    Update settings.py

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Donde están tus archivos estáticos originales (desarrollo)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),

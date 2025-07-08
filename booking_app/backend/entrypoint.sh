3b7938cb8a09642eac666ac9100a258948e52643
Ever Cifuentes <31419663+EverBilly@users.noreply.github.com>
2025-07-08 20:26:10 +0200

    Update entrypoint.sh

--- booking_app/backend/entrypoint.sh
+++ booking_app/backend/entrypoint.sh
@@ -2,19 +2,21 @@
 
 set -e
 
+# Crear carpeta staticfiles antes de que Whitenoise la busque
+mkdir -p /app/staticfiles
+
 # Mostrar contenido de static/ para depuración
 echo "Archivos en static/:"
 ls -R /app/static/
 
-mkdir -p staticfiles
-
+# Recolectar archivos estáticos
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 # Aplicar migraciones
-echo "Aplicando migraciones..."
+echo "Applying migrations..."
 python manage.py migrate --noinput
 
 # Iniciar servidor
-echo "Iniciando servidor..."
+echo "Starting server..."
 exec gunicorn --bind 0.0.0.0:$PORT booking_project.wsgi:application
